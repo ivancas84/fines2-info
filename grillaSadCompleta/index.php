11a63b4008afaff6c4a638e67aaf11f8aa48574d
unknown <ivancas84@gmail.com>
2019-08-19 13:38:41 -0300

    agregar opcion para imprimir la grilla de un solo curso

--- grillaSadCompleta/index.php
+++ grillaSadCompleta/index.php
@@ -11,19 +11,20 @@ $fechaSemestre = (!empty($_GET["fecha_semestre"])) ? $_GET["fecha_semestre"] : 1
 $dependencia = (!empty($_GET["dependencia"])) ? $_GET["dependencia"] : null;
 $comision = (!empty($_GET["comision"])) ? $_GET["comision"] : null;
 $pendiente = (!empty($_GET["pendiente"])) ? true : false;
+$curso = (!empty($_GET["curso"])) ? $_GET["curso"] : false;
 
-$filtros = [
-    ["com_fecha_anio", "=", $fechaAnio],
-    ["com_fecha_semestre", "=", $fechaSemestre],
-    ["com_dvi__clasificacion_nombre","=","Fines"],
-    ["com_publicar","=",true],
-    //["horario","=",true]
-];
-
-if($pendiente) array_push($filtros, ["toma_activa", "=", false]);
-if($dependencia) array_push($filtros, ["com_dvi_sed_dependencia", "=", id_dependencia_numero($dependencia)]);
-if($comision) array_push($filtros, ["com_id", "=", $comision]);
-
+$filtros = [];
+if($curso){
+    array_push($filtros, ["id", "=", $curso]);
+} else {
+    array_push($filtros, ["com_fecha_anio", "=", $fechaAnio]);
+    array_push($filtros, ["com_fecha_semestre", "=", $fechaSemestre]);
+    array_push($filtros, ["com_dvi__clasificacion_nombre","=","Fines"]);
+    array_push($filtros, ["com_publicar","=",true]);
+    if($pendiente) array_push($filtros, ["toma_activa", "=", false]);
+    if($dependencia) array_push($filtros, ["com_dvi_sed_dependencia", "=", id_dependencia_numero($dependencia)]);
+    if($comision) array_push($filtros, ["com_id", "=", $comision]);
+}
 
 $render = new Render();
 $render->setCondition($filtros);
